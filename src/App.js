168fe2c5ca950152745e59365ef0c0561b58935a
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-04-06 18:41:19 +0200

    F01-Routes: Define all routes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,30 @@
+import { Outlet, createBrowserRouter } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./components/pages/Home";
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/", element: <Home /> },
+      { path: "/", element: <Home /> },
+      { path: "/", element: <Home /> },
+    ],
+  },
+]);
+
 function App() {
   return <div className="font-bold underline">App</div>;
 }
