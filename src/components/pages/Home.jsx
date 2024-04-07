8df2ebfbcf6c4499752db6d82fc852b5e098d3b9
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-04-07 22:10:17 +0200

    🚧 F02-Strapi: use useFetch to test data which from strapi api

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+import useFetch from "../../hooks/useFetch";
 
 export default function Home() {
+  const { data } = useFetch("/products?populate=*");
+  console.log(data);
   return <div>Home</div>;
 }
--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { request } from "../request";
 
 const useFetch = (url) => {
   const [data, setData] = useState(null);
