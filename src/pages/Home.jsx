67b02ea6e99a9e594bee783b833d5d088b1b9d0f
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-04-08 13:34:19 +0200

    ♻️   F02-Strapi: Create LatestProduct

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,10 @@
 import React from "react";
-import useFetch from "../hooks/useFetch";
-import ProductSlider from "../components/ProductSlider";
+import LatestProducts from "../components/LatestProducts";
 
 export default function Home() {
-  const { data } = useFetch("/products?populate=*&filters[isNew]=true");
   return (
-    <div className="mb-16">
-      <div className="container mx-auto">
-        <h2 className="h2 mb-6 text-center xl:text-left">Latest Products</h2>
-      </div>
-      <ProductSlider data={data} />
-    </div>
+    <section>
+      <LatestProducts />
+    </section>
   );
 }
