ff6116512a20909a48e7e2e976c20fba57c02e66
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-04-08 11:48:50 +0200

    🚧   F02-Strapi: add pagination and navigation to the swiper

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -5,10 +5,11 @@ export default function Product({ product }) {
   console.log(product);
   return (
     <Link>
-      <div className="group">
+      <div className="bg-gradient-to-t from-[#1F2126] to-[#33363D] drop-shadow-2xl w-full h-[362px] rounded-[8px] overflow-hidden relative group">
+        <div className="mb-4">new</div>
         <div className="w-full h-[200px] flex items-center justify-center relative">
           <img
-            className="w-[170px] h-[170px] group-hover:scale-90 transition-all"
+            className="mb-12 w-[170px] h-[170px] group-hover:scale-90 transition-all"
             src={`http://localhost:1337${product.attributes.image.data.attributes.url}`}
             alt=""
           />
--- src/components/ProductSlider.jsx
+++ src/components/ProductSlider.jsx
@@ -3,6 +3,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
 import "swiper/css/pagination";
+import "../slider.css";
 
 import { Navigation, Pagination } from "swiper/modules";
 
@@ -10,7 +11,33 @@ import Product from "./Product";
 
 export default function ProductSlider({ data }) {
   return (
-    <Swiper>
+    <Swiper
+      modules={[Pagination, Navigation]}
+      loop={false}
+      navigation={true}
+      breakpoints={{
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 30,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 30,
+        },
+        1024: {
+          slidesPerView: 2,
+          spaceBetween: 30,
+        },
+        1440: {
+          slidesPerView: 5,
+          spaceBetween: 30,
+        },
+      }}
+      pagination={{
+        clickable: true,
+      }}
+      className="mt-20 productSlider mx-auto max-w-[360px] md:max-w-lg xl:max-w-[1410px]"
+    >
       <>
         {data?.map((product) => {
           return (
