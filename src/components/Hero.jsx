86db4f0fc8f416724d780ccb55aad6e60904095a
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-04-09 13:47:32 +0200

    🚧    F02-Strapi: Add promo section to Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,19 +8,51 @@ export default function Hero() {
       <div className="container mx-auto">
         <div className="flex flex-col gap-y-[30px] xl:flex-row xl:gap-x-[30px]">
           {/* sidebar */}
-          <div className="w-full bg-red-500 max-w-lg mx-auto">
+          <div>
             <CategoryNav />
           </div>
           {/* main slider */}
           <div className="w-full max-w-lg bg-pink-300 lg:max-w-[734px] mx-auto">
             <MainSlider />
           </div>
-          {/* promo imgs */}
+          {/* promo */}
           <div className="flex flex-col gap-y-[30px] w-full bg-blue-500 max-w-lg mx-auto h-[500px]">
-            {/* promo img 1 */}
-            <div className="">promo 1</div>
-            {/* promo img 2 */}
-            <div>promo 2</div>
+            {/* promo 1 */}
+            <div className="bg-gradient-to-t from-[#1F2126] to-[#33363D] drop-shadow-2xl flex-1 h-[250px] rounded-[8px] overflow-hidden relative p-6">
+              {/* text */}
+              <div className="flex flex-col max-w-[144px] h-full justify-center">
+                <div className="text-[20px] uppercase font-medium leading-tight mb-4">
+                  Save 35% all Piano{" "}
+                </div>
+                <a href="#" className="uppercase text-yellow-300">
+                  Shop now
+                </a>
+              </div>
+              {/* image */}
+              <img
+                className="absolute z-20 -top-2 -right-0"
+                src={"/images/Design_sans_titre__2_-removebg-preview.png"}
+                alt=""
+              />
+            </div>
+            {/* promo 2 */}
+            <div className="bg-gradient-to-t from-[#1F2126] to-[#33363D] drop-shadow-2xl flex-1 h-[250px] rounded-[8px] overflow-hidden relative p-6">
+              {/* text */}
+              <div className="flex flex-col max-w-[144px] h-full justify-center">
+                <div className="text-[20px] uppercase font-medium leading-tight mb-4">
+                  Save 25% all Drums{" "}
+                </div>
+                <a href="#" className="uppercase text-yellow-300">
+                  Shop now
+                </a>
+              </div>
+              {/* image */}
+              <img
+                className="absolute z-20 -top-2 -right-0"
+                src={"/images/Design_sans_titre__2_-removebg-preview.png"}
+                alt=""
+              />
+            </div>
           </div>
         </div>
       </div>
