9fbe6343e076dbf1478b28b8b5aeb7b627d9b8d5
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-04-10 21:07:01 +0200

    🚧  F04-CartContext: add onCick event to close CategoryNavMobile

--- src/components/CategoryNavMobile.jsx
+++ src/components/CategoryNavMobile.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import { FiX } from "react-icons/fi";
 
 export default function CategoryNavMobile({ setCatNavMobile }) {
-  return <div className="w-full h-full bg-black p-8">CategoryNavMobile</div>;
+  return (
+    <div className="w-full h-full bg-black p-8">
+      <div
+        onClick={() => setCatNavMobile(false)}
+        className="flex justify-end mb-8 cursor-pointer"
+      >
+        <FiX className="text-3xl" />
+      </div>
+    </div>
+  );
 }
