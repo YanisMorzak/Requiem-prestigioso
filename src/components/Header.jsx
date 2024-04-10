e0383c0f6ec0079f8eae1ce051bed64cd5263466
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-04-10 16:01:28 +0200

    🚧  F03-Header: Create Cart, CategoryNavMobile and SearchForm

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,49 @@
 import React from "react";
+import SearchForm from "./SearchForm";
+
+import { FiMenu } from "react-icons/fi";
+import { SlBag } from "react-icons/sl";
+import CategoryNavMobile from "./CategoryNavMobile";
+import { Link } from "react-router-dom";
+import Cart from "./Cart";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container mx-auto">
+        <div>
+          {/* menu */}
+          <div>
+            <FiMenu />
+          </div>
+          {/* category nav mobile*/}
+          <div>
+            <CategoryNavMobile />
+          </div>
+          {/* logo */}
+          <Link to={"/"}>
+            <img src="/images/Logo-RP.png" alt="logo" />
+          </Link>
+          {/* searchform - show on desktop only */}
+          <div className="hidden w-full xl:flex xl:max-w-[734px]">
+            <SearchForm />
+          </div>
+          {/* cart icon*/}
+          <div className="relative cursor-pointer">
+            <SlBag className="text-2xl" />
+            {/* amount */}
+            <div>2</div>
+          </div>
+          {/* cart */}
+          <div>
+            <Cart />
+          </div>
+        </div>
+        {/* searchform - show on mobile only */}
+        <div className="lg:hidden">
+          <SearchForm />
+        </div>
+      </div>
+    </header>
+  );
 }
