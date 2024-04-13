76644fe88c6b2ebb0fb80999b48d67825059550a
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-04-13 15:59:40 +0200

    🚧      F04-CartContext: display the clicked category title above the products

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CategoryNav from "../components/Hero/CategoryNav";
 import { useParams } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
@@ -11,7 +11,14 @@ export default function Products() {
   const { data } = useFetch(
     `/products?populate=*&filters[categories][id][$eq]=${id}`
   );
-  console.log(data);
+  const [title, setTitle] = useState(null);
+
+  // set the title when the data is fetched
+  useEffect(() => {
+    if (data) {
+      setTitle(data[0].attributes.categories.data[0].attributes.title);
+    }
+  });
   return (
     <div className="mb-16 pt-40 lg:pt-2 px-5">
       <div className="container mx-auto">
@@ -19,7 +26,9 @@ export default function Products() {
           <CategoryNav />
           <main className="flex-1">
             {/* title */}
-            <div>title</div>
+            <div className="py-3 text-xl uppercase text-center lg:text-left">
+              {title}
+            </div>
             {/* product grid */}
             <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-[15px] md:gap-[30px] ">
               {data?.map((product) => {
