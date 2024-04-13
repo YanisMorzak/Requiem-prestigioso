39edad8427af640643f8d9a13bc8c05874224ce3
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-04-13 12:45:46 +0200

    🚧   F04-CartContext: display products searched

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,16 +1,31 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
+import Product from "../components/LatestProduct/Product";
 
 export default function Search() {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const searchTerm = searchParams.get("query");
 
+  // get products based on search term
   const { data } = useFetch(
     `/products?populate=*&filters[title][$contains]=${searchTerm}`
   );
   console.log(data);
 
-  return <div>Search</div>;
+  return (
+    <div>
+      <div className="container mx-auto">
+        <div>
+          {/* products grid */}
+          <div>
+            {data?.map((product) => {
+              return <Product product={product} key={product.id} />;
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
