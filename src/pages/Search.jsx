ef9a782b5bd93910032ed4902f514f7d2ee7984e
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-04-13 12:34:32 +0200

    🚧   F04-CartContext: get products based on search term

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
 
 export default function Search() {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const searchTerm = searchParams.get("query");
-  console.log(searchTerm);
+
+  const { data } = useFetch(
+    `/products?populate=*&filters[title][$contains]=${searchTerm}`
+  );
+  console.log(data);
+
   return <div>Search</div>;
 }
