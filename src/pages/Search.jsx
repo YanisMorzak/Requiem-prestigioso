8e8b3deb071607c1ada89f8f4ca9d91f4e7db4cb
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-04-13 12:11:18 +0200

    🚧   F04-CartContext: retrieve tapper value from input in searchTerm

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 export default function Search() {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const searchTerm = searchParams.get("query");
+  console.log(searchTerm);
   return <div>Search</div>;
 }
