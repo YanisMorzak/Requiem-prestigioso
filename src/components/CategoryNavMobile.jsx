ccc3cb3c4c404f5d090ea820543ac5549160b4b8
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-04-13 14:41:54 +0200

    🚧    F04-CartContext: add categories in CategoryNavMobile and their links

--- src/components/CategoryNavMobile.jsx
+++ src/components/CategoryNavMobile.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { FiX } from "react-icons/fi";
+import useFetch from "../hooks/useFetch";
+import { Link } from "react-router-dom";
 
 export default function CategoryNavMobile({ setCatNavMobile }) {
+  const { data } = useFetch("/categories");
   return (
     <div className="w-full h-full bg-black p-8">
       <div
@@ -9,6 +12,19 @@ export default function CategoryNavMobile({ setCatNavMobile }) {
         className="flex justify-end mb-8 cursor-pointer"
       >
         <FiX className="text-3xl" />
+      </div>
+      <div className="flex flex-col gap-y-8">
+        {data?.map((category) => {
+          return (
+            <Link
+              to={`products/${category.id}`}
+              className="uppercase font-medium"
+              key={category.id}
+            >
+              {category.attributes.title}
+            </Link>
+          );
+        })}
       </div>
     </div>
   );
