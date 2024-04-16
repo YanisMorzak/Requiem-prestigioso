8d949800c9764f3576d7ad46df130109cab3658f
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-04-16 11:46:05 +0200

    🚧    F06-Cart: add onClick event to icon click

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import { IoClose } from "react-icons/io5";
+import { CartContext } from "../context/CartContext";
 
 export default function Cart() {
+  const { setIsOpen } = useContext(CartContext);
   return (
-    <div className="px-4">
+    <div className="w-full h-full px-4">
       <div>
         {/* close icon */}
-        <div className="text-4xl w-20 h-[98px] flex justify-start items-center cursor-pointer">
+        <div
+          onClick={() => setIsOpen(false)}
+          className="text-4xl w-20 h-[98px] flex justify-start items-center cursor-pointer"
+        >
           <IoClose />
         </div>
       </div>
