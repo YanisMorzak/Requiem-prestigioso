0b3579a0337d1359f3a34808b6a684715883b50c
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-04-16 11:32:14 +0200

    🚧    F06-Cart: add close icon

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 
+import { IoClose } from "react-icons/io5";
+
 export default function Cart() {
-  return <div>Cart</div>;
+  return (
+    <div className="px-4">
+      <div>
+        {/* close icon */}
+        <div className="text-4xl w-20 h-[98px] flex justify-start items-center cursor-pointer">
+          <IoClose />
+        </div>
+      </div>
+    </div>
+  );
 }
