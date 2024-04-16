4bdae11d3ca6cb1ed6ed6c3c04616603c0eda640
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-04-16 11:01:04 +0200

    💄   Forgetten-F05-ProductDetails: change the background color to the product image

--- src/components/ProductDetails/ProductDetailsImage.jsx
+++ src/components/ProductDetails/ProductDetailsImage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function ProductDetailsImage({ data }) {
   return (
-    <div className="md:max-w-[40%] lg:h-[540px] bg-emeraude rounded-[8px] overflow-hidden drop-shadow-2xl flex justify-center p-5">
+    <div className="md:max-w-[40%] lg:h-[540px] bg-gradient-to-r from-teal-900 to-teal-600 rounded-[8px] overflow-hidden drop-shadow-2xl flex justify-center p-5">
       <img
         src={`http://localhost:1337${data[0].attributes.image.data.attributes.url}`}
         alt={data[0].attributes.title}
