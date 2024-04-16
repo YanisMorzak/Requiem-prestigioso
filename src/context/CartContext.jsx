08d98dc203a0be2791e625f4043437a6880a0618
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-04-16 14:49:26 +0200

    ✨    F06-Cart: check if item is already in the cart

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -5,12 +5,30 @@ export const CartContext = createContext();
 export default function CartProvider({ children }) {
   const [isOpen, setIsOpen] = useState(false);
   const [cart, setCart] = useState([]);
+  const [amount, setAmount] = useState(0);
 
   const addToCart = (item, id) => {
     const itemID = parseInt(id);
     const newItem = { ...item[0], amount: 1 };
     setCart([...cart, newItem]);
-    console.log(cart);
+
+    // check if item is already in the cart
+    const isAlreadyInCart = cart.find((item) => item.id === itemID);
+
+    if (isAlreadyInCart) {
+      const newCart = cart.map((item) => {
+        if (item.id === itemID) {
+          setAmount(isAlreadyInCart.amount + 1);
+          return { ...item, amount: isAlreadyInCart.amount + 1 };
+        } else {
+          return item;
+        }
+      });
+      setCart(newCart);
+    } else {
+      setCart([...cart, newItem]);
+    }
+    setIsOpen(true);
   };
 
   return (
