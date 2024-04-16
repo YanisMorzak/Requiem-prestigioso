57ecc08d0bc5189da2047504daed20d14a5d7c88
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-04-16 12:12:41 +0200

    ✨    F06-Cart: add addToCart function which add a product to the shopping cart

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -4,8 +4,17 @@ export const CartContext = createContext();
 
 export default function CartProvider({ children }) {
   const [isOpen, setIsOpen] = useState(false);
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (item, id) => {
+    const itemID = parseInt(id);
+    const newItem = { ...item[0], amount: 1 };
+    setCart([...cart, newItem]);
+    console.log(cart);
+  };
+
   return (
-    <CartContext.Provider value={{ isOpen, setIsOpen }}>
+    <CartContext.Provider value={{ isOpen, setIsOpen, addToCart }}>
       {children}
     </CartContext.Provider>
   );
