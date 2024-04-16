eea08c794d2a1bdae7d2ac04a7c0b8d28b1bbda0
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-04-16 16:22:37 +0200

    🚧   F06-Cart: display the total amount of a product by quantity

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,27 +1,33 @@
 import React from "react";
 import { IoClose } from "react-icons/io5";
 import { Link } from "react-router-dom";
+import CartItemQuantity from "./CartItemQuantity";
 
 export default function CartItem({ item }) {
   console.log("item: ", item);
   return (
-    <div className="flex">
+    <div className="flex gap-x-8">
       <Link to={`/product/${item.id}`} className="w-[70px] h-[70px]">
         <img
           src={`http://localhost:1337${item.attributes.image.data.attributes.url}`}
           alt=""
         />
       </Link>
-      <div>
+      <div className="flex-1">
         {/* title & remove icon */}
-        <div>
+        <div className="flex">
           <Link to={`/product/${item.id}`}>{item.attributes.title}</Link>
           <div className="cursor-pointer text-[24px] hover:text-accent transition-all">
             <IoClose />
           </div>
         </div>
         {/* quantity */}
-        <div>quantity</div>
+        <div>
+          <div>
+            <CartItemQuantity item={item} />
+          </div>
+          <div>$ {item.attributes.price * item.amount}</div>
+        </div>
         {/* price */}
         <div>
           <span className="text-accent">
