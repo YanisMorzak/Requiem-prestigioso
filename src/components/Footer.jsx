f88857fa702d7752d039a400df6b0e53c4289f1b
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-04-17 19:12:34 +0200

    💄   F07-Footer: add style to all footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,15 +6,17 @@ export default function Footer() {
   return (
     <footer className="pt-16 px-5">
       <div className="container mx-auto">
-        <div>
-          <h2>Subscribe to our newsletter</h2>
-          <p>
+        <div className="text-center">
+          <h2 className="text-3xl capitalize mb-6">
+            Subscribe to our newsletter
+          </h2>
+          <p className="text-white/70">
             Be the first to get the latest news about trends, promotion and much
             more!
           </p>
         </div>
         {/* form */}
-        <form>
+        <form className="w-full max-w-3xl mx-auto flex flex-col md:flex-row gap-5 my-12">
           <input
             type="text"
             placeholder="Your email adress"
@@ -25,7 +27,7 @@ export default function Footer() {
           </button>
         </form>
         {/* links */}
-        <div>
+        <div className="text-base text-white/60 flex gap-x-6 capitalize max-w-max mx-auto mb-9 md:mb-6">
           <a href="#" className="hover:text-white transition-all">
             Returns policy
           </a>
@@ -37,7 +39,7 @@ export default function Footer() {
           </a>
         </div>
         {/* socials */}
-        <div>
+        <div className="flex gap-x-6 max-w-max mx-auto text-xl mb-16">
           <a href="#" className="hover:text-white text-light transition-all">
             <FaLinkedin />
           </a>
