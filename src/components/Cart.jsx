67af03e979f3df5101cda08814eebecde9564b9b
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-04-17 10:30:45 +0200

    🚧      F06-Cart: add buttons to Cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,12 +4,13 @@ import { IoClose } from "react-icons/io5";
 import { CartContext } from "../context/CartContext";
 import CartItem from "./CartItem";
 import Total from "./Total";
+import Button from "./Button";
 
 export default function Cart() {
   const { setIsOpen, cart } = useContext(CartContext);
   return (
     <div className="w-full h-full px-4">
-      <div className="overflow-y-auto overflow-x-hidden h-[80vh] ">
+      <div className="overflow-y-auto overflow-x-hidden h-[77vh] ">
         {/* close icon */}
         <div
           onClick={() => setIsOpen(false)}
@@ -25,6 +26,17 @@ export default function Cart() {
       </div>
       {/* subtotal & total */}
       <Total />
+      {/* buttons */}
+      <div className="px-6">
+        {cart.length >= 1 ? (
+          <div className="flex justify-between gap-x-4">
+            <Button btnValue={"Clear Cart"} />
+            <Button btnValue={"Checkout"} />
+          </div>
+        ) : (
+          <div>Your cart is empty</div>
+        )}
+      </div>
     </div>
   );
 }
