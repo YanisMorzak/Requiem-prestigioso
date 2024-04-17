0712c0f271e63f2075810923d52f07909fdd4794
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-04-17 18:33:24 +0200

    🚧  F07-Footer: add form, links & socials icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <footer className="pt-16 ">
+    <footer className="pt-16 px-5">
       <div className="container mx-auto">
         <div>
           <h2>Subscribe to our newsletter</h2>
@@ -12,6 +14,37 @@ export default function Footer() {
           </p>
         </div>
         {/* form */}
+        <form>
+          <input
+            type="text"
+            placeholder="Your email adress"
+            className="h-[50px] rounded-[8px] w-full outline-none px-6 text-primary"
+          />
+          <button className="h-[50px] flex justify-center items-center rounded-[8px] px-10 py-[10px] text-sm uppercase font-bold bg-extraLight text-primary hover:bg-accent-hover transition-all min-w-[150px]">
+            Join
+          </button>
+        </form>
+        {/* links */}
+        <div>
+          <a href="#" className="hover:text-white transition-all">
+            Returns policy
+          </a>
+          <a href="#" className="hover:text-white transition-all">
+            Track youy order
+          </a>
+          <a href="#" className="hover:text-white transition-all">
+            Shipping & Delivery
+          </a>
+        </div>
+        {/* socials */}
+        <div>
+          <a href="#" className="hover:text-white text-light transition-all">
+            <FaLinkedin />
+          </a>
+          <a href="#" className="hover:text-white text-light transition-all">
+            <FaGithub />
+          </a>
+        </div>
       </div>
     </footer>
   );
