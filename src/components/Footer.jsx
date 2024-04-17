095917949d45b2eb01e1659f31e800a9cc4a4195
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-04-17 17:53:21 +0200

    🚧  F06-Cart: add title and text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="pt-16 ">
+      <div className="container mx-auto">
+        <div>
+          <h2>Subscribe to our newsletter</h2>
+          <p>
+            Be the first to get the latest news about trends, promotion and much
+            more!
+          </p>
+        </div>
+        {/* form */}
+      </div>
+    </footer>
+  );
 }
