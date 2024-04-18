71f01f9ac049f40fc8fd17079df466b06750ef59
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-04-18 17:59:18 +0200

    🚧 : add good links for socials icon

--- src/components/Footer/SocialIconFooter.jsx
+++ src/components/Footer/SocialIconFooter.jsx
@@ -4,10 +4,18 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 export default function SocialIconFooter() {
   return (
     <div className="flex gap-x-6 max-w-max mx-auto text-xl mb-16">
-      <a href="#" className="hover:text-white text-light transition-all">
+      <a
+        href="https://www.linkedin.com/in/yanis-morzak/"
+        target="_blank"
+        className="hover:text-white text-light transition-all"
+      >
         <FaLinkedin />
       </a>
-      <a href="#" className="hover:text-white text-light transition-all">
+      <a
+        href="https://github.com/YanisMorzak/Requiem-prestigioso"
+        target="_blank"
+        className="hover:text-white text-light transition-all"
+      >
         <FaGithub />
       </a>
     </div>
